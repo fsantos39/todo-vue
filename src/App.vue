2806d2c1d479d214168678ec258bb2934e1584d2
Fernando Santos <f.bsantos2505@gmail.com>
2024-09-30 08:58:37 -0300

    componentizando a aplicação

--- src/App.vue
+++ src/App.vue
@@ -58,15 +58,9 @@ const cadastraTarefa = () => {
 
 <template>
   <div class="container">
-    <Cabecalho  :tarefas-pendentes="10"/>
-    <Formulario />
-    <ListaDeTarefas />
+    <Cabecalho  :tarefas-pendentes="getTarefasPendentes().length"/>
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
     
   </div>
 </template>
-
-<style scoped>
-.done {
-  text-decoration: line-through;
-}
-</style>
--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -1,6 +1,10 @@
+<script setup>
+    const props = defineProps(['tarefas']);
+</script>
+
 <template>
     <ul class="list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
             <input
                 @change="(evento) => (tarefa.finalizada = evento.target.checked)"
                 :checked="tarefa.finalizada"
@@ -17,3 +21,9 @@
         </li>
     </ul>
 </template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
